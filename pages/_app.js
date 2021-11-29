8c54a84ddb634e286949090fc03a7d80d6e52ada
zixin-lin <linzixin101@gmail.com>
2021-11-29 11:11:16 -0500

    netlify2

--- pages/_app.js
+++ pages/_app.js
@@ -3,9 +3,11 @@ import Layout from '../lib/Layout'
 
 function MyApp({ Component, pageProps }) {
   return (
+    <AuthContextProvider>
     <Layout>
       <Component {...pageProps} />
     </Layout>
+    </AuthContextProvider>
   )
 }
 
---
+++ stores/authContext.js
@@ -0,0 +1,42 @@
+import { createContext, useState, useEffect } from 'react'
+import netlifyIdentity from 'netlify-identity-widget'
+
+const AuthContext = createContext({
+    user: null,
+    login: () => {},
+    logout: () => {},
+    authReady: false
+  })
+
+  export const AuthContextProvider = ({ children }) => {
+    const [user, setUser] = useState(null)
+  
+    useEffect(() => {
+      netlifyIdentity.on('login', (user) => {
+        setUser(user)
+        netlifyIdentity.close()
+        console.log('login event')
+      })
+  
+      // init netlify identity connection
+      netlifyIdentity.init()
+  
+      return () => {
+        netlifyIdentity.off('login')
+      }
+    }, [])
+  
+    const login = () => {
+      netlifyIdentity.open()
+    }
+  
+    const context = { user, login }
+  
+    return (
+      <AuthContext.Provider value={context}>
+        { children }
+      </AuthContext.Provider>
+    )
+  }
+  
+  export default AuthContext
