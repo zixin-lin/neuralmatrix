631d62f668ca53dcb9a3e7fad631c32d39667b71
zixin-lin <linzixin101@gmail.com>
2021-11-29 11:33:53 -0500

    netlify3

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
 import '../styles/globals.css'
 import Layout from '../lib/Layout'
-
+import { AuthContextProvider } from '../stores/authContext'
 function MyApp({ Component, pageProps }) {
   return (
     <AuthContextProvider>
