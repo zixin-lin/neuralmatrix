1d005b4d1f60e3dd6c5b283c3ab7d9d4aae8a67e
zixin-lin <linzixin101@gmail.com>
2021-11-29 13:29:51 -0500

    netlify8

--- pages/ninjias.js
+++ pages/ninjias.js
@@ -1,11 +1,57 @@
-export default function ninjias(){
-    return(
-        <div>
-            <h1>test login function {
-                
-            }</h1>
+import { useContext, useEffect, useState } from 'react'
+import styles from '../styles/Guides.module.css'
+import AuthContext from '../stores/authContext'
+
+export default function Guides() {
+  const { user, authReady, login } = useContext(AuthContext)
+  const [guides, setGuides] = useState(null)
+  const [error, setError] = useState(null)
+
+  useEffect(() => {
+    if (authReady) {
+      fetch('/.netlify/functions/guides', user && {
+        headers: {
+          Authorization:  'Bearer ' + user.token.access_token
+        }
+      })
+      .then(res => {
+        if (!res.ok) {
+          login()
+          throw Error('You must be logged in to view this content')
+        }
+        return res.json()
+      })
+      .then(data => {
+        setError(null)
+        setGuides(data)
+      })
+      .catch(err => {
+        setError(err.message)
+        setGuides(null)
+      })
+    }
+
+  },[user, authReady])
+
+  return (
+    <div className={styles.guides}>
+      
+      {!authReady && <div>Loading...</div>}
+
+      {error && (
+        <div className={styles.error}>
+          <p>{ error }</p>
         </div>
-    )
+      )}
 
+      {guides && guides.map(guide => (
+        <div key={guide.title} className={styles.card}>
+          <h3>{ guide.title }</h3>
+          <h4>written by {guide.author}</h4>
+          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. At corrupti iste ab magnam dignissimos id maxime rerum quae minima. Delectus maxime culpa est consequatur veritatis, perspiciatis cum corrupti possimus quis?</p>
+        </div>
+      ))}
 
+    </div> 
+  )
 }
