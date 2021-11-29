a79497e7b1989cac3fcca5822c2d5cb7a0568168
zixin-lin <linzixin101@gmail.com>
2021-11-29 11:52:28 -0500

    netlify5

--- stores/authContext.js
+++ stores/authContext.js
@@ -2,41 +2,49 @@ import { createContext, useState, useEffect } from 'react'
 import netlifyIdentity from 'netlify-identity-widget'
 
 const AuthContext = createContext({
-    user: null,
-    login: () => {},
-    logout: () => {},
-    authReady: false
-  })
+  user: null,
+  login: () => {},
+  logout: () => {},
+  authReady: false
+})
 
-  export const AuthContextProvider = ({ children }) => {
-    const [user, setUser] = useState(null)
-  
-    useEffect(() => {
-      netlifyIdentity.on('login', (user) => {
-        setUser(user)
-        netlifyIdentity.close()
-        console.log('login event')
-      })
-  
-      // init netlify identity connection
-      netlifyIdentity.init()
-  
-      return () => {
-        netlifyIdentity.off('login')
-      }
-    }, [])
-  
-    const login = () => {
-      netlifyIdentity.open()
+export const AuthContextProvider = ({ children }) => {
+  const [user, setUser] = useState(null)
+
+  useEffect(() => {
+    netlifyIdentity.on('login', (user) => {
+      setUser(user)
+      netlifyIdentity.close()
+      console.log('login event')
+    })
+    netlifyIdentity.on('logout', () => {
+      setUser(null)
+      console.log('logout event')
+    })
+
+    // init netlify identity connection
+    netlifyIdentity.init()
+
+    return () => {
+      netlifyIdentity.off('login')
     }
-  
-    const context = { user, login }
-  
-    return (
-      <AuthContext.Provider value={context}>
-        { children }
-      </AuthContext.Provider>
-    )
+  }, [])
+
+  const login = () => {
+    netlifyIdentity.open()
   }
-  
-  export default AuthContext
+
+  const logout = () => {
+    netlifyIdentity.logout()
+  }
+
+  const context = { user, login, logout }
+
+  return (
+    <AuthContext.Provider value={context}>
+      { children }
+    </AuthContext.Provider>
+  )
+}
+
+export default AuthContext
